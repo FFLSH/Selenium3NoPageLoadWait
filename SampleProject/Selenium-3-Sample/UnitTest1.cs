76504a6f12d2036aa168b1200bc4767bc629c347
Fraser Lowndes <fraserlowndes@etech.net>
2016-11-03 16:05:01 +0000

    Adding Selenium 2.53.1 and Selenium 3 sample projects with sample test

---
+++ SampleProject/Selenium-3-Sample/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Selenium_3_Sample
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Environment.SetEnvironmentVariable("PATH", Path.GetFullPath("../../"));
+
+            IWebDriver fireFoxDriver = new FirefoxDriver();
+
+            fireFoxDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            fireFoxDriver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(30));
+
+            fireFoxDriver.Navigate().GoToUrl("http://google.com");
+
+            Assert.AreEqual("Google", fireFoxDriver.Title);
+
+            fireFoxDriver.FindElement(By.LinkText("Images")).Click();
+
+            Assert.AreEqual("Google Images", fireFoxDriver.Title);
+        }
+    }
+}
---
+++ SampleProject/Selenium2-53-1Sample/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Selenium2_53_1Sample
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver fireFoxDriver = new FirefoxDriver();
+
+            fireFoxDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            fireFoxDriver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(30));
+
+            fireFoxDriver.Navigate().GoToUrl("http://google.com");
+
+            Assert.AreEqual("Google", fireFoxDriver.Title);
+
+            fireFoxDriver.FindElement(By.LinkText("Images")).Click();
+
+            Assert.AreEqual("Google Images", fireFoxDriver.Title);
+        }
+    }
+}
